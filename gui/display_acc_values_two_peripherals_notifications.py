f701fcc3748e115638092f486d5485d42a2be99e
xabeab <xavier.beaule@polymtl.ca>
2019-04-03 14:17:37 -0400

    Added a class in the backend for the bluetooth devices. The connection function is completed

--- gui/display_acc_values_two_peripherals_notifications.py
+++ gui/display_acc_values_two_peripherals_notifications.py
@@ -8,34 +8,18 @@ import sys
 import ctypes as ct
 import time
 
-bytes_sets=[[i*j for j in range (18)] for i in range(10)]
-sets=[[i*j for j in range (8)] for i in range(10)]
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
     def handleNotification(self, cHandle, data):
         data_unpacked=unpack('hhhhhhIH', data)
-#        for i in range(0,10):
-#            for j in range (0,18):
-#                bytes_sets[i][j]=data_unpacked[j+18*i]
-#        for i in range(0,10):
-#            sets[i][0]=ct.c_short(bytes_sets[i][0] + bytes_sets[i][1]*256)
-#            sets[i][1]=ct.c_short(bytes_sets[i][2] + bytes_sets[i][3]*256)
-#            sets[i][2]=ct.c_short(bytes_sets[i][4] + bytes_sets[i][5]*256)
-#            sets[i][3]=ct.c_short(bytes_sets[i][6] + bytes_sets[i][7]*256)
-#            sets[i][4]=ct.c_short(bytes_sets[i][8] + bytes_sets[i][9]*256)
-#            sets[i][5]=ct.c_short(bytes_sets[i][10] + bytes_sets[i][11]*256)
-#            sets[i][6]=ct.c_uint(bytes_sets[i][12]*256 + bytes_sets[i][13]*65536+ bytes_sets[i][15])
-#            sets[i][7]=ct.c_ushort(bytes_sets[i][16] + bytes_sets[i][17]*256)
-#        for i in sets:
-#            print(i)
         print(data_unpacked);
             
 BlueNRG_1_MAC="F1:E5:A8:F0:96:80"
 BlueNRG_2_MAC="D1:D3:22:72:05:69"
 
-sensor_serive_UUID='02366e80-cf3a-11e1-9ab4-0002a5d5c51b'
+sensor_service_UUID='02366e80-cf3a-11e1-9ab4-0002a5d5c51b'
 
 acc_UUID='340a1b80-cf4b-11e1-ac36-0002a5d5c51b'
 start_UUID='2c41cc24-cf13-11e1-4fdf-0002a5d5c51b'
--- gui/gui.py
+++ gui/gui.py
@@ -1,17 +1,53 @@
 from tkinter import *
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from bluepy import btle
+from struct import *
+
+
+BlueNRG_MAC = ["F1:E5:A8:F0:96:80", "E4:69:61:26:E6:23"]
+sensor_service_UUID = '02366e80-cf3a-11e1-9ab4-0002a5d5c51b'
+acc_UUID = '340a1b80-cf4b-11e1-ac36-0002a5d5c51b'
 
 
 root = Tk()
-root.title("CHUM's seizure detection application")
+root.title("Multimodal Seizure Detection")
+# root.iconbitmap('snake.ico')
+root.geometry("1000x600")
+root.resizable(0, 0)
 
+class BluetoothDevice:
+    def __init__(self, adressmac):
+        self.service = 0
+        self.char = 0
+        self.peripheral = btle.Peripheral(adressmac, btle.ADDR_TYPE_RANDOM)
+
+
+class MyDelegate(btle.DefaultDelegate):
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        data_unpacked=unpack('hhhhhhIH', data)
+        print(data_unpacked)
 
 def connectProcedure():
-    print("Connecting...")
-
+    BlueNRG_devices = []
+    for deviceNumber, macAdress in enumerate(BlueNRG_MAC):
+        print("Connecting to BlueNRG -", deviceNumber+1, "...")
+        BlueNRG = BluetoothDevice(BlueNRG_MAC[deviceNumber])
+        BlueNRG_devices.append(BlueNRG)
+        BlueNRG_devices[deviceNumber].peripheral.setMTU(200)
+        BlueNRG_devices[deviceNumber].peripheral.setDelegate(MyDelegate())
+        print("BlueNRG2 -", deviceNumber+1, " Services...")
+        for svc in BlueNRG_devices[deviceNumber].peripheral.services:
+            print(str(svc))
+        print("BlueNRG2 -", deviceNumber+1, " Characteristics...")
+        BlueNRG_devices[deviceNumber].service = BlueNRG_devices[deviceNumber].peripheral.getServiceByUUID(sensor_service_UUID)
+        BlueNRG_devices[deviceNumber].char= BlueNRG_devices[deviceNumber].service.getCharacteristics(acc_UUID)[0]
+    print("The connection procedure was successful")
 
 def startProcedure():
     print("Starting...")
@@ -23,24 +59,25 @@ def chooseSaveDirectory():
 # Creating main frame
 mainFrame = Frame(root, width=500, height=500)
 mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
+root.columnconfigure(0, weight=0)
+root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
 
 # Buttons
-connectButton = Button(mainFrame, text="CONNECT", bg="orange", fg="white", command=connectProcedure)
-connectButton.grid(row=0, column=0, padx=5, pady=100)
-startButton = Button(mainFrame, text="SYNC & START", bg="orange", fg="white", command=startProcedure)
-startButton.grid(row=1, column=0, padx=5, pady=5)
-saveButton = Button(mainFrame, text="CHOOSE SAVE DIRECTORY", bg="orange", fg="white", command=chooseSaveDirectory)
-saveButton.grid(row=2, column=0, padx=5, pady=100)
+connectButton = Button(mainFrame, text="CONNECT", bg="orange", fg="white", command=connectProcedure, padx=20, pady=20)
+connectButton.grid(row=0, column=0, padx=20, pady=100)
+startButton = Button(mainFrame, text="SYNC & START", bg="orange", fg="white", command=startProcedure, padx=20, pady=20)
+startButton.grid(row=1, column=0, padx=20, pady=5)
+saveButton = Button(mainFrame, text="CHOOSE SAVE DIRECTORY", bg="orange", fg="white", command=chooseSaveDirectory, padx=20, pady=20)
+saveButton.grid(row=2, column=0, padx=20, pady=100)
 
-# Plotting
+# Plot Initialization
 f = Figure(figsize=(5, 5), dpi=100)
 a = f.add_subplot(111)
-a.plot([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 6, 1, 3, 8, 9, 3, 5, 4])
+a.plot([0], [0])
 canvas = FigureCanvasTkAgg(f, master=root)
 canvas.draw()
-canvas.get_tk_widget().grid(row=0, column=1)
+canvas.get_tk_widget().grid(row=0, column=1, sticky=(N, W, E, S))
 
 
 root.mainloop()
