0f6c06d92ad85b448973fe6f6954a1ff348cf758
xabeab <xavier.beaule@polymtl.ca>
2019-04-05 15:13:11 -0400

    Dummy data is displayed in RT for three sets

--- gui/gui.py
+++ gui/gui.py
@@ -8,11 +8,13 @@ from multiprocessing import Pool, Array, Process
 from bluepy import btle
 from struct import *
 import sys
+import matplotlib.animation as animation
 
 macAdresses = ('F1:E5:A8:F0:96:80', 'E4:69:61:26:E6:23')
 sensor_serive_UUID = '02366e80-cf3a-11e1-9ab4-0002a5d5c51b'
 acc_UUID = '340a1b80-cf4b-11e1-ac36-0002a5d5c51b'
 processes = [Process() for count in macAdresses]
+data = Array('h', 12)
 
 root = Tk()
 root.title("Multimodal Seizure Detection Utility")
@@ -24,7 +26,7 @@ class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
-    def handleNotification(self, cHandle, data):
+    def handleNotification(self, cHandle, data):         #Need to distinguish between pheripherals
         data_unpacked=unpack('hhhhhhIH', data)
         print(data_unpacked)
 
@@ -57,7 +59,6 @@ def run_process(adress, data):
 def connectProcedure():
     # Create shared memory
     global processes
-    data = Array('h', 12)
     print("Connecting the devices, syncing and starting...")
     for idx, name in enumerate(macAdresses):
         process = Process(target=run_process, args=(macAdresses[idx], data))
@@ -71,6 +72,30 @@ def disconnectProcedure():
 
 def chooseSaveDirectory():
     print("Choose save directory...")
+
+def animate(i, ys1, ys2, ys3):
+    global data
+
+    # Add y to list
+    acc_x = data[0]
+    acc_y = data[1]
+    acc_z = data[2]
+
+    ys1.append(acc_x)
+    ys2.append(acc_y)
+    ys3.append(acc_z)
+
+    # Limit y list to set number of items
+    ys1 = ys1[-x_len:]
+    ys2 = ys2[-x_len:]
+    ys3 = ys3[-x_len:]
+
+    # Update line with new Y values
+    line[0].set_ydata(ys1)
+    line[1].set_ydata(ys2)
+    line[2].set_ydata(ys3)
+
+    return line
 
 # Creating main frame
 mainFrame = Frame(root, width=500, height=500)
@@ -88,13 +113,40 @@ saveButton = Button(mainFrame, text="CHOOSE SAVE DIRECTORY", bg="orange", fg="wh
 saveButton.grid(row=2, column=0, padx=20, pady=100)
 
 # Plot Initialization
+# Parameters
+x_len = 300         # Number of points to display
+y_range = [-2000, 2000]  # Range of possible Y values to display
+xs = list(range(0, x_len))
+ys1 = [0] * x_len
+ys2 = [0] * x_len
+ys3 = [0] * x_len
+
 f = Figure(figsize=(5, 5), dpi=100)
 a = f.add_subplot(111)
-a.plot([0], [0])
+# Create a blank line. We will update the line in animate
+line1, = a.plot(xs, ys1)
+line2, = a.plot(xs, ys2)
+line3, = a.plot(xs, ys3)
+line = [line1, line2, line3]
+a.set_ylim(y_range)
+a.set_title('Device 1 Data')
+a.set_xlabel('Showing the last 300 samples')
+
 canvas = FigureCanvasTkAgg(f, master=root)
 canvas.draw()
 canvas.get_tk_widget().grid(row=0, column=1, sticky=(N, W, E, S))
 
-root.mainloop()
+
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(f, animate, fargs=(ys1, ys2, ys3,), interval=50, blit=False)
+
+
+
+
+
+while True:
+    root.update_idletasks()
+    root.update()
 
 
